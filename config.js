aaaccd3333f402bc008a1d45216dab330e9673c3
ramosalex630 <ramosalex630@gmail.com>
2026-01-27 19:19:24 -0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://national-sapphire-6jahkmrjny.edgeone.app/Olivia_Dean_-_So_Easy_(mp3.pm).mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dglgyqzu8/video/upload/v1769570250/Olivia_Dean_-_So_Easy__mp3.pm_ic5sug.mp3", // Music streaming URL
         startText: "🎵 Ponle Play",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.7                        // Volume level (0.0 to 1.0)
